da66bedb84b140115894dad38a532d65556e2eaf
minaamq <102026701+minaamq@users.noreply.github.com>
2024-07-20 14:18:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+var docleng=document.querySelectorAll("button").length;
+for(var i=0;i<docleng;i++){
+document.querySelectorAll("button")[i].addEventListener("click",handleclick)
+function handleclick(){
+    var keyy=this.innerHTML;
+    makesound(keyy);
+    animm(keyy);
+  
+}}
+
+
+document.addEventListener("keypress", function(event){
+
+    makesound(event.key);
+    animm(event.key);
+
+})
+
+function makesound(key){
+
+
+    switch(key){
+        case"w":
+        var tom1=new Audio("sounds/tom-1.mp3")
+        tom1.play();
+        break;
+        case "a":
+        var tom2=new Audio("sounds/tom-2.mp3")
+        tom2.play();
+        break;
+        case "s":
+        var tom3=new Audio("sounds/tom-3.mp3")
+        tom3.play();
+        break;
+        case "d":
+        var tom4=new Audio("sounds/tom-4.mp3")
+        tom4.play();
+        break;
+        case "j":
+        var snare=new Audio("sounds/snare.mp3")
+        snare.play();
+        break;
+        case "k":
+        var crash=new Audio("sounds/crash.mp3")
+        crash.play();
+        break;
+        case "l":
+        var kick=new Audio("sounds/kick-bass.mp3")
+        kick.play();
+        break;
+
+
+    }
+
+
+
+}
+function animm(keyye){
+    var currbutton =document.querySelector("." + keyye)
+    currbutton.classList.add("pressed");
+
+    setTimeout(function() {
+        currbutton.classList.remove("pressed");
+    }, 100);
+
+
+
+}
